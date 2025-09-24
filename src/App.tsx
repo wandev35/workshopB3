3f4aa96492aefd0d839ff9e8b66979b9b4392873
erwan.forveille <e.forveille.mail@gmail.com>
2025-09-24 17:56:31 +0200

    dernière maj Front End 24/09/2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Accueil from "./pages/Accueil";
@@ -10,28 +13,32 @@ import Listeserveurs from "./pages/Listesserveurs";
 import ChatPage from "./pages/ChatPage";
 import Inscription from "./pages/Inscription";
 
+// Exemple d’une page pour tester le backend FastAPI
+import TestAPI from "./pages/TestAPI";
+
+// Création du client React Query
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-          <Route path="accueil" element={<Accueil />} />
-          <Route path="inscription" element={<Inscription />} />
-          <Route path="chat" element={<ChatPage />} />
-          <Route path="serveurs" element={<Listeserveurs />} 
-          />
-          
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="accueil" element={<Accueil />} />
+            <Route path="inscription" element={<Inscription />} />
+            <Route path="chat" element={<ChatPage />} />
+            <Route path="serveurs" element={<Listeserveurs />} />
+            <Route path="test-api" element={<TestAPI />} /> {/* route pour tester le backend */}
+            <Route path="*" element={<NotFound />} /> {/* catch-all */}
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
